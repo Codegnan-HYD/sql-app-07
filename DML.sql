8be53afab2fb329e69faf4b9b3e541f4e09f2892
Sandy1005 <polavarapusandy777@gmail.com>
2026-07-03 23:46:08 +0530

    ERD

---
+++ DML.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO parking_spots (spot_number, basement_level) VALUES 
+(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), 
+(1, 2), (2, 2), (3, 2), (4, 2), (5, 2), 
+(1, 3), (2, 3), (3, 3), (4, 3), (5, 3);
+
+
+INSERT INTO employees (first_name, last_name, parking_spot_id) VALUES 
+('Sandeep', 'Kumar', 201), ('Padma', 'sri', NULL), ('sai', 'suresh', 202), ('sai', 'krishna', NULL), ('sai', 'kumar', 203), 
+('ram', 'Kumar', 206), ('thrinadh', 'Ram', NULL), ('Harshitha', 'hridya', 207), ('raju', 'Singh', NULL), ('rajesh', 'Kumar', 208), 
+('punneth', 'Singh', 211), ('Dinesh', 'Kumar', NULL), ('Ankit', 'Verma', 212), ('Harshit', 'Singh', NULL), ('Anuj', 'Katiyal', NULL);
